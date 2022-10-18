ce367deff8cdbf24fb22ccae94b2417ae036615c
NarayanAdithya <narayanadithya1234@gmail.com>
2022-10-18 13:23:20 +0530

    Added Backend

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -28,7 +28,7 @@ def make_session():
     engine=""
     while(not engine):
         try:
-            engine = create_engine(os.environ.get('POSTGRESQL_URL'))
+            engine = create_engine(os.environ.get('POSTGRESQL_URL',""))
             # Creating Database If Not Exists
             if not database_exists(engine.url):
                 create_database(engine.url)
@@ -51,15 +51,15 @@ def callback(ch, method, properties, body):
             global s
             unserialized=pickle.loads(body)
             single_row = pd.DataFrame(unserialized['row']).T
-            t = Transaction(time=int(single_row['Time']), amount=float(single_row['Amount']))
+            t = Transaction(time=int(single_row['Time']), amount=float(single_row['Amount']))   # type: ignore # type: ignore
             try:
                 single_row.drop(['Amount','Class'],axis=1,inplace=True)
             except Exception as e:
                 print(f"Error {e} while dropping columns")
-            answer=model.predict(single_row)[0]
-            t.Class=int(answer)
-            s.add(t)
-            s.commit()
+            answer=model.predict(single_row)[0]  # type: ignore
+            t.Class=int(answer)  # type: ignore
+            s.add(t)  # type: ignore
+            s.commit()  # type: ignore
 
 def main():
         # Get DB Session
@@ -72,7 +72,7 @@ def main():
         # Declaring Exchanges and Bindings
         channel = connection.channel()
         channel.queue_declare('CreditCardData')
-        channel.exchange_declare(exchange='fraudDetection',exchange_type='direct')
+        channel.exchange_declare(exchange='fraudDetection',exchange_type='direct')  # type: ignore
         channel.queue_bind('CreditCardData','fraudDetection','CreditCardData')
 
         # Consuming
--- consumer/start.sh
+++
@@ -1,2 +0,0 @@
-python /usr/src/app/app.py  &
-python /usr/src/app/consumer.py --restart
